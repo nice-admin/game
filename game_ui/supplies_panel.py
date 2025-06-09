efd9110e928da5807b19c6919a55be73d9048b80
Hurricane-046 <rzeifart@gmail.com>
2025-06-09 22:03:49 +0200

    multiple supplies panel support

--- game_ui/supplies_panel.py
+++ game_ui/supplies_panel.py
@@ -13,13 +13,14 @@ UNFOLDED_HEIGHT = 300
 
 
 class PanelContent:
-    def __init__(self, header, lines=None, font=None, header_font=None):
+    def __init__(self, header, lines=None, font=None, header_font=None, icon_path=None):
         self.header = header
         self.lines = lines if lines is not None else []
         self.font = font or get_font1(24)
         self.header_font = header_font or get_font1(36)
         self.header_color = adjust_color(BASE_COL, white_factor=0.0, exposure=5)
         self.text_color = (0, 0, 0)
+        self.icon_path = icon_path
 
     def draw(self, surface, x, y, icon_width=0, icon_height=0):
         # Use SUPPLIES_PANEL_WIDTH as left margin for header, and 10px top margin
@@ -36,7 +37,7 @@ class PanelContent:
 
 
 class ExpandingPanel:
-    def __init__(self, x, y_ratio, button_size, expanded_size, animation_duration=0.07, content=None):
+    def __init__(self, x, y_ratio, button_size, expanded_size, animation_duration=0.07, content=None, icon_path=None):
         self.button_width, self.button_height = button_size
         self.expanded_width, self.expanded_height = expanded_size
         self.x = x
@@ -48,12 +49,13 @@ class ExpandingPanel:
         self.current_width = self.button_width
         self.current_height = self.button_height
         self.font = pygame.font.SysFont(None, 24)
+        self.icon_path = icon_path
         if isinstance(content, PanelContent):
             self.content = content
         elif isinstance(content, (list, tuple)) and content:
-            self.content = PanelContent(content[0], content[1:], self.font)
+            self.content = PanelContent(content[0], content[1:], self.font, icon_path=icon_path)
         else:
-            self.content = PanelContent("", [], self.font)
+            self.content = PanelContent("", [], self.font, icon_path=icon_path)
 
     def handle_event(self, event, surface):
         y = int(surface.get_size()[1] * self.y_ratio)
@@ -98,7 +100,7 @@ class ExpandingPanel:
         pygame.draw.rect(surface, UI_BG1_COL, panel_rect, border_radius=ROUNDING)
         pygame.draw.rect(surface, UI_BORDER1_COL, panel_rect, width=3, border_radius=ROUNDING)
         from game_core.config import resource_path
-        icon_path = resource_path('data/graphics/supplies_panel/supplies.png')
+        icon_path = self.icon_path or resource_path('data/graphics/supplies_panel/supplies.png')
         icon = None
         icon_size = min(self.button_width, self.button_height) - 16
         icon_x = self.x + (self.button_width - icon_size) // 2
@@ -121,20 +123,85 @@ class ExpandingPanel:
             surface.blit(temp_surf, (self.x - left_shift, y))
         return panel_rect
 
-# --- Legacy API for compatibility ---
-_sliding_panel_instance = ExpandingPanel(
-    SUPPLIES_PANEL_X,
-    SUPPLIES_PANEL_Y_RATIO,
-    (FOLDED_WIDTH, FOLDED_HEIGHT),
-    (UNFOLDED_WIDTH, UNFOLDED_HEIGHT),
-    content=PanelContent("Supplies:", ["- Food", "- Water", "- Tools"])
-)
+# --- Multiple panels setup ---
+panel_configs = [
+    {
+        'header': 'Supplies:',
+        'lines': ['- Food', '- Water', '- Tools'],
+        'icon_path': 'data/graphics/supplies_panel/supplies.png',
+    },
+    {
+        'header': 'Equipment:',
+        'lines': ['- Radios', '- Flashlights', '- Batteries'],
+        'icon_path': 'data/graphics/supplies_panel/equipment.png',
+    },
+    {
+        'header': 'Medical:',
+        'lines': ['- Bandages', '- Medicine', '- First Aid'],
+        'icon_path': 'data/graphics/supplies_panel/medical.png',
+    },
+    {
+        'header': 'Tools:',
+        'lines': ['- Hammer', '- Wrench', '- Screwdriver'],
+        'icon_path': 'data/graphics/supplies_panel/tools.png',
+    },
+    {
+        'header': 'Misc:',
+        'lines': ['- Rope', '- Tape', '- Glue'],
+        'icon_path': 'data/graphics/supplies_panel/misc.png',
+    },
+]
+
+def create_panels(surface):
+    panels = []
+    panel_spacing = FOLDED_HEIGHT * 1.1
+    screen_height = surface.get_height()
+    for i, cfg in enumerate(panel_configs):
+        y_ratio = SUPPLIES_PANEL_Y_RATIO + i * (panel_spacing / screen_height)
+        panel = ExpandingPanel(
+            SUPPLIES_PANEL_X,
+            y_ratio,
+            (FOLDED_WIDTH, FOLDED_HEIGHT),
+            (UNFOLDED_WIDTH, UNFOLDED_HEIGHT),
+            content=PanelContent(cfg['header'], cfg['lines'], icon_path=cfg['icon_path']),
+            icon_path=cfg['icon_path']
+        )
+        panels.append(panel)
+    return panels
+
+# Remove old panels list and creation
+panels = []  # will be set in init or first draw
 
 def handle_supplies_panel_event(event, surface):
-    return _sliding_panel_instance.handle_event(event, surface)
+    global panels
+    if not panels or len(panels) != len(panel_configs):
+        panels = create_panels(surface)
+    clicked_idx = None
+    for idx, panel in enumerate(panels):
+        if panel.handle_event(event, surface):
+            clicked_idx = idx
+            break
+    if clicked_idx is not None:
+        # Collapse all other panels
+        for j, other_panel in enumerate(panels):
+            if j != clicked_idx and other_panel.expanded:
+                other_panel.expanded = False
+                other_panel.animating = True
+                other_panel.anim_start_time = pygame.time.get_ticks()
+        # Move the clicked panel to the end of the list (drawn last, on top)
+        panels.append(panels.pop(clicked_idx))
+        return True
+    return False
 
 def update_supplies_panel_animation():
-    _sliding_panel_instance.update_animation()
+    for panel in panels:
+        panel.update_animation()
 
 def draw_supplies_panel(surface):
-    return _sliding_panel_instance.draw(surface)
+    global panels
+    if not panels or len(panels) != len(panel_configs):
+        panels = create_panels(surface)
+    rects = []
+    for panel in panels:
+        rects.append(panel.draw(surface))
+    return rects
